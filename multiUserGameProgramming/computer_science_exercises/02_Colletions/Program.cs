6156feeae40dc277374b34e58a6b49cdd2d0645c
Bruce J <s12157624@students.duvalschools.org>
2023-10-05 13:53:01 -0400

    Project Update -- Kelley

---
+++ multiUserGameProgramming/computer_science_exercises/02_Colletions/Program.cs
@@ -0,0 +1,29 @@
+﻿// 02_Collections, Bruce Johnson, 10/3/24 v0.1a
+using System;
+
+namespace _02_Colletions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Collections are variables that can store multiple values in one variable.
+
+            /* Arrays
+            -- Number of elements in an array CANNOT change.
+            -- Contents of elements in an array CAN change.
+            -- Items in the array are called ELEMENTS.
+            -- Arrays are ordered, meaning each item has a fixed position.
+            -- The poosition is known as the INDEX.
+            -- First element in an array is inde 0.
+            */
+
+            // Declaring and Defining an Array
+            string[] breakfastFoods = {"Bacon", "Waffles", "Pancakes", "Cereal", "Parfait"};
+            int[] testScores = {95, 100, 25, 15, 27, 35};
+            float[] GPA = {3.14f, 2.25f, 1.74f, 1.99f, 0.99f, 4.25f};
+
+            
+        }
+    }
+}
--- multiUserGameProgramming/gamingExercises/00_numberGuess/numberGuess.cs
+++ multiUserGameProgramming/gamingExercises/00_numberGuess/numberGuess.cs
@@ -1,4 +1,4 @@
-// Bruce Johnson, Number Guess, v0.4
+// Bruce Johnson, Number Guess, v0.5
 /*
 Generate secret number from a defined range of numbers (i.e. 0-10, 0-50, 0-100). 
 Print game instructions including range and num. of guesses allowed. 
@@ -34,7 +34,8 @@ namespace numberGuess
            int secretNumber = -1;
            int numGuesses = 0; // Number of guesses player is ALLOWED.
            int numAttempts = 0; // Number of guesses TAKEN.
-           int playerScore = 0:
+           int playerGuess = 0;
+           int playerScore = 0;
            int cpuScore = 0;
            string difficulty = "";
            int rangeMin = -1;
@@ -44,30 +45,71 @@ namespace numberGuess
            Console.WriteLine("Easy Mode: Range is 0 - 10 with 4 gueses.\nNormal Mode: Range is 0 - 25 with 3 gueses.\nHard Mode is 0 - 50 with 2 guesses.\n")
 
            // DIFFICULTY SELECTION
-           Console.WriteLine("Please type Easy, Normal, or Hard and press ENDRER.")
+           Console.WriteLine("Please type Easy, Normal, or Hard and press ENDRER.");
            difficulty = Console.ReadLine();
-           // Console.ReadLine() will save to STRING by default
-           Console.WriteLine("You have selected" + difficulty);
+           // Console.ReadLine() will save to STRING by default.
+           Console.WriteLine("You have selected " + difficulty);
            if (difficulty == "Easy") {
                rangeMin = 0;
                rangeMax = 10;
                numGuesses = 4
-
-           } else if (NORMAL MODE) {
+           } else if (difficulty == "Normal") {
                rangeMin = o;
                rangemax = 25;
                numGuesses = 3;
-
-           } else if (HARD MODE) {
+           } else if (difficulty == "Hard") {
                rangeMin = 0;
                rangeMax = 50;
                numGuesses = 2;
            } else {
-
+               // Code to run if no difficulty is selected.
+               Console.WriteLine("No difficulty slescted correctly, defaulting to Normal.\n");
+               rangeMin = 0;
+               rangeMax = 25;
+               rangeGuesses = 3;      
            }
            Console.WriteLine("Minimum: " + rangeMin);
            Console.Wrieline("Maximum: " + rangeMax);
            Console.WriteLine("Num. Guesses: " + numberGuesses);
+
+           // START THE MATCH!
+           while (playerScore != 3 && cpuScore != 3) {
+               // Any code you want to run BEFORE eac hround goes here.
+               // GENERATE SERET NUMBER
+               Random rndNum = new Random(); 
+               secretNumber = rndNum.Next(rangeMin, rangeMax);
+               Console.WriteLine("player Score: " + playerScore + "\n");
+               Console.WriteLine("CPU Score: " + cpuScore + "\n");
+               // START EACH ROUND
+               for ( int i = 0; i , numGuesses ; i ++) {
+                   // Code to guess number goes here.
+                   Console.WriteLine("You have used " + numAttempts + " this round.\n");
+                   Console.WriteLine("You must guess between " + rangeMin + "and " +  rangeMax + ".\n");
+                   playerGuess = System.Convert.ToInt32(Console.ReadLine());
+                   if (playerGuess == secretNumber) {
+                       // Print a seccess message!
+                       playerScore++;
+                       break;
+                   } else {
+                       if (playerGuess > secretNumber) {
+                           Console.WriteLine("Your guess is too high!\n");
+                       } else {
+                           Console.WriteLine("Your guess is too low!\n");
+                       }
+                   }
+                   numAttempts++;
+               }
+               if (playerGuess != secretNumber) {
+                   cpuScore++;
+                   // Print a round lost message to the console
+               } 
+           }
+           if (playerScore >= 3) {
+               Console.WriteLine("You have won the game!\n");
+           } else {
+               Console.WriteLine("You have lost the game!\n");
+           }
+
         }
 
     }
