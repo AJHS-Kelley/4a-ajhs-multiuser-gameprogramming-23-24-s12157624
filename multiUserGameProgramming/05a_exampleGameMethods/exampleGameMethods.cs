133d98540ad89d1374143d1a991304e5dd3d1418
Bruce J <90473269+s12157624@users.noreply.github.com>
2023-12-07 13:32:04 -0500

    v0.2

---
+++ multiUserGameProgramming/05a_exampleGameMethods/exampleGameMethods.cs
@@ -0,0 +1,30 @@
+// Bruce Johnson, Example Game Methods, v0.1
+using System;
+
+namespace UPDATEFOREACHPROGRAM
+{
+    class UPDATEFOREACHPROGRAM
+    
+    {
+        static float playerhealth()
+        // Ammount Of health the that the player has after every move / attack 
+        { 
+            
+        }
+
+        static int shieldPoints()
+        {
+            
+        }
+
+        static string playerAttack()
+        {
+
+        }
+
+        static string chestLoot()
+        {
+            
+        }
+    }
+}
